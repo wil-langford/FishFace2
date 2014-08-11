db6b06104d16d732375db2dcd6775e95beeec89d
wil-langford <wil.langford+github@gmail.com>
2014-08-11 10:21:40 -0700

    Some automatically generated, database-related things.

--- django_fishface/djff/migrations/0001_initial.py
+++ django_fishface/djff/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(SchemaMigration):
         # Adding model 'Experiment'
         db.create_table(u'djff_experiment', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('experiment_name', self.gf('django.db.models.fields.CharField')(max_length=250, null=True)),
+            ('experiment_name', self.gf('django.db.models.fields.CharField')(max_length=250, null=True, blank=True)),
             ('experiment_start_dtg', self.gf('django.db.models.fields.DateTimeField')()),
             ('experiment_last_viewed', self.gf('django.db.models.fields.DateTimeField')()),
         ))
@@ -24,13 +24,30 @@ class Migration(SchemaMigration):
             ('dtg_capture', self.gf('django.db.models.fields.DateTimeField')()),
             ('species', self.gf('django.db.models.fields.CharField')(max_length=50)),
             ('voltage', self.gf('django.db.models.fields.FloatField')(default=0)),
-            ('filename', self.gf('django.db.models.fields.FilePathField')(max_length=100, null=True, blank=True)),
-            ('orientation', self.gf('django.db.models.fields.SmallIntegerField')(default=None, null=True, blank=True)),
-            ('location', self.gf('django.db.models.fields.CommaSeparatedIntegerField')(max_length=20, null=True, blank=True)),
+            ('filename', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal(u'djff', ['Image'])
+
+        # Adding model 'ImageAnalysis'
+        db.create_table(u'djff_imageanalysis', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('image', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['djff.Image'])),
+            ('analysis_dtg', self.gf('django.db.models.fields.DateTimeField')()),
+            ('orientation', self.gf('django.db.models.fields.SmallIntegerField')(default=None)),
+            ('location', self.gf('djff.fields.LocationField')(default=0)),
+            ('silhouette', self.gf('djff.fields.ContourField')(default=0)),
             ('verified_dtg', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
             ('verified_by', self.gf('django.db.models.fields.CharField')(max_length=100, null=True, blank=True)),
         ))
-        db.send_create_signal(u'djff', ['Image'])
+        db.send_create_signal(u'djff', ['ImageAnalysis'])
+
+        # Adding model 'HopperChain'
+        db.create_table(u'djff_hopperchain', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('hopperchain_name', self.gf('django.db.models.fields.CharField')(max_length=250, blank=True)),
+            ('hopperchain_spec', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal(u'djff', ['HopperChain'])
 
 
     def backwards(self, orm):
@@ -40,27 +57,46 @@ class Migration(SchemaMigration):
         # Deleting model 'Image'
         db.delete_table(u'djff_image')
 
+        # Deleting model 'ImageAnalysis'
+        db.delete_table(u'djff_imageanalysis')
+
+        # Deleting model 'HopperChain'
+        db.delete_table(u'djff_hopperchain')
+
 
     models = {
         u'djff.experiment': {
             'Meta': {'object_name': 'Experiment'},
             'experiment_last_viewed': ('django.db.models.fields.DateTimeField', [], {}),
-            'experiment_name': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True'}),
+            'experiment_name': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'}),
             'experiment_start_dtg': ('django.db.models.fields.DateTimeField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        u'djff.hopperchain': {
+            'Meta': {'object_name': 'HopperChain'},
+            'hopperchain_name': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'}),
+            'hopperchain_spec': ('django.db.models.fields.TextField', [], {}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
         },
         u'djff.image': {
             'Meta': {'object_name': 'Image'},
             'dtg_capture': ('django.db.models.fields.DateTimeField', [], {}),
             'experiment': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['djff.Experiment']"}),
-            'filename': ('django.db.models.fields.FilePathField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'filename': ('django.db.models.fields.TextField', [], {}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'location': ('django.db.models.fields.CommaSeparatedIntegerField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
-            'orientation': ('django.db.models.fields.SmallIntegerField', [], {'default': 'None', 'null': 'True', 'blank': 'True'}),
             'species': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'verified_by': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
-            'verified_dtg': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
             'voltage': ('django.db.models.fields.FloatField', [], {'default': '0'})
+        },
+        u'djff.imageanalysis': {
+            'Meta': {'object_name': 'ImageAnalysis'},
+            'analysis_dtg': ('django.db.models.fields.DateTimeField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['djff.Image']"}),
+            'location': ('djff.fields.LocationField', [], {'default': '0'}),
+            'orientation': ('django.db.models.fields.SmallIntegerField', [], {'default': 'None'}),
+            'silhouette': ('djff.fields.ContourField', [], {'default': '0'}),
+            'verified_by': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'verified_dtg': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'})
         }
     }
 
