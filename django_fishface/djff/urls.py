2dc745e9d86db5c1af212ad780e96360153193f3
wil-langford <wil.langford+github@gmail.com>
2014-08-13 15:54:25 -0700

    Manually created HopperChain editor part done.

--- django_fishface/djff/urls.py
+++ django_fishface/djff/urls.py
@@ -4,4 +4,39 @@ import djff.views as views
 urlpatterns = dcu.patterns(
     '',
     dcu.url(r'^$', views.index, name='index'),
+    dcu.url(
+        r'^hc/$',
+        views.hopperchain_index,
+        name='hopperchain_index'
+    ),
+    dcu.url(
+        r'^hc/(?P<chain_id>\d+)/$',
+        views.hopperchain_detail,
+        name='hopperchain_detail'
+    ),
+    dcu.url(
+        r'^hc/edit/(?P<chain_id>\d+)/$',
+        views.hopperchain_edit,
+        name='hopperchain_edit'
+    ),
+    dcu.url(
+        r'^hc/rename/(?P<chain_id>\d+)/$',
+        views.hopperchain_rename,
+        name='hopperchain_rename'
+    ),
+    dcu.url(
+        r'^hc/renamer/(?P<chain_id>\d+)/$',
+        views.hopperchain_renamer,
+        name='hopperchain_renamer'
+    ),
+    dcu.url(
+        r'^hc/edit/(?P<chain_id>\d+)/delete/(?P<hopper_index>\d+)/$',
+        views.hopperchain_delete,
+        name='hopperchain_delete'
+    ),
+    dcu.url(
+        r'^hc/edit/(?P<chain_id>\d+)/insert/(?P<hopper_index>\d+)/$',
+        views.hopperchain_insert,
+        name='hopperchain_insert'
+    ),
 )
--- django_fishface/djff/views.py
+++ django_fishface/djff/views.py
@@ -1,12 +1,77 @@
 import django.shortcuts as ds
+import django.http as dh
+import django.core.urlresolvers as dcu
 
 from djff.models import Experiment, Image, ImageAnalysis
 from djff.models import HopperChain
 
-
 def index(request):
+    pass
+
+
+def hopperchain_edit(request, chain_id):
+    chain = ds.get_object_or_404(HopperChain, pk=chain_id)
+    return ds.render(
+        request,
+        'djff/hopperchain_edit.html',
+        {'chain': chain}
+    )
+
+
+def hopperchain_detail(request, chain_id):
+    chain = ds.get_object_or_404(HopperChain, pk=chain_id)
+    return ds.render(
+        request,
+        'djff/hopperchain_detail.html',
+        {'chain': chain}
+    )
+
+
+def hopperchain_index(request):
     hopperchain_list = HopperChain.objects.all().order_by(
         'hopperchain_name'
     )
     context = {'hopperchain_list': hopperchain_list}
-    return ds.render(request, 'djff/index.html', context)
+    return ds.render(request, 'djff/hopperchain_index.html', context)
+
+
+def hopperchain_delete(request, chain_id, hopper_index):
+    chain = ds.get_object_or_404(HopperChain, pk=chain_id)
+    hopper_index = int(hopper_index)
+
+    del chain.hopperchain_spec[hopper_index]
+    chain.save()
+
+    return dh.HttpResponseRedirect(dcu.reverse('djff:hopperchain_edit',
+                                               args=(chain.id,)))
+
+
+def hopperchain_insert(request, chain_id, hopper_index):
+    chain = ds.get_object_or_404(HopperChain, pk=chain_id)
+    hopper_index = int(hopper_index)
+
+    chain.hopperchain_spec.insert(hopper_index, ('null', dict()))
+    chain.save()
+
+    return dh.HttpResponseRedirect(dcu.reverse('djff:hopperchain_edit',
+                                               args=(chain.id,)))
+
+
+def hopperchain_renamer(request, chain_id):
+    chain = ds.get_object_or_404(HopperChain, pk=chain_id)
+
+    chain.hopperchain_name = request.POST['new_name']
+    chain.save()
+
+    return dh.HttpResponseRedirect(dcu.reverse('djff:hopperchain_edit',
+                                               args=(chain.id,)))
+
+
+def hopperchain_rename(request, chain_id):
+    chain = ds.get_object_or_404(HopperChain, pk=chain_id)
+    return ds.render(
+        request,
+        'djff/hopperchain_rename.html',
+        {'chain': chain}
+    )
+
