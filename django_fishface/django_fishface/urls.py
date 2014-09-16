bf4355c607e2af4b886f9384b7fe830acfd0ad12
wil-langford <wil.langford+github@gmail.com>
2014-09-16 10:37:31 -0700

    Pointed base URL at the Experiment index.

--- django_fishface/django_fishface/urls.py
+++ django_fishface/django_fishface/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns(
     # url(r'^$', 'django_fishface.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'djff.views.xp_index', name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^fishface/', include('djff.urls', namespace='djff')),
 )
