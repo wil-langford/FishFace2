46cdb824ec3c96bd43659a659de014cb3407619d
Wil Langford <wil.langford+github@gmail.com>
2014-12-15 10:03:44 -0800

    Updated requirements.

--- cluster/fishface_cluster.py
+++ cluster/fishface_cluster.py
@@ -9,16 +9,11 @@ print "I am rank {}.".format(rank)
 if rank == 0:
     # I get to be in charge!  I'm so excited!
 
-
-
     for i in range(size):
         data = comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG)
         print "I see drone {}.".format(data['rank'])
 
-
-
 else:
     # I'm a drone.
     print "I'm rank {} and I'm telling the boss about myself.".format(rank)
-    comm.send({'rank': rank}, dest=0, tag=rank);
-
+    comm.send({'rank': rank}, dest=0, tag=rank)
