a8811936e5cbe8407345dd9c4e3e09624282dc89
Wil Langford <wil.langford+github@gmail.com>
2015-01-23 17:01:28 -0800

    Split out some of the common code between the cq builder and cq interface.

--- django_fishface/djff/static/djff/cq_builder.js
+++ django_fishface/djff/static/djff/cq_builder.js
@@ -1,126 +1,12 @@
 $(document).ready(function(){
-    function data_attrib_from_job_spec(job_spec) {
-        return job_spec.voltage + "_" +
-               job_spec.current + "_" +
-               job_spec.startup_delay + "_" +
-               job_spec.interval + "_" +
-               job_spec.duration;
-    }
-
-    function job_spec_from_data_attrib(data_attrib) {
-        var job_spec_array = data_attrib.split("_");
-        var job_spec = {
-            voltage: job_spec_array[0],
-            current: job_spec_array[1],
-            startup_delay: job_spec_array[2],
-            interval: job_spec_array[3],
-            duration: job_spec_array[4]
-        };
-
-        return job_spec;
-    }
-
-    function li_from_queue(queue) {
-        return '<li class="saved_queue" id="CJQ_' + queue.id + '">' +
-            'Name: ' + queue.name + '<br>' +
-            'Description: ' + queue.comment + '<br><br>' +
-            '<span class="boxed queue_loader" id="CJQ_' + queue.id + '_LOAD">&larr;load</span> ' +
-            '<span class="boxed queue_deleter" id="CJQ_' + queue.id + '_DEL">delete</span>' +
-            '</li>';
-    }
-
-    function li_from_job_spec(job_spec) {
-        return '<li class="job_queue_item" data-attrib_job_spec=' + data_attrib_from_job_spec(job_spec) + '>' +
-               inner_li_from_job_spec(job_spec) +
-               '</li>';
-    }
-
-    function inner_li_from_job_spec(job_spec) {
-        var inner_li;
-        if (job_spec.interval > 0) {
-            inner_li =
-                'Set (' + job_spec.voltage + 'V, ' + job_spec.current + 'A), ' +
-                'wait ' + job_spec.startup_delay + 's, ' +
-                'then <br /> capture every ' +
-                job_spec.interval + 's for ' + job_spec.duration + 's';
-        } else {
-            inner_li = 'Set (' + job_spec.voltage + 'V, ' + job_spec.current + 'A) ' + 'for ' + job_spec.duration + 's';
-        }
-        return inner_li;
-    }
-
-    function refresh_queues() {
-        $.ajax({
-            type: 'GET',
-            url: window.ff.cjqs_url,  // set by inline javascript on the main page
-            success: function (data, status, jqXHR) {
-                window.ff.cjqs = data.cjqs;
-                window.ff.cjq_ids = data.cjq_ids;
-                var cjq_list = $('#cjq_list');
-                cjq_list.html('');
-                if (data.cjq_ids.length > 0) {
-                    for (var i in data.cjq_ids) {
-                        var queue = data.cjqs[data.cjq_ids[i]];
-                        cjq_list.append(li_from_queue(queue));
-                    }
-                } else {
-                    cjq_list.html('<li>No saved queues.<li>');
-                }
-                
-            },
-            error: function(jqXHR, status, error) {
-
-            },
-            dataType: 'json'
-        });
-    }
-
-    function get_queue_array() {
-        var queue_attrib_array = $('#capture_queue_builder').sortable('toArray', {'attribute': 'data-attrib_job_spec'});
-        var queue_array = [];
-        for (var i in queue_attrib_array) {
-            if (queue_attrib_array[i].length > 0) {
-                queue_array.push(job_spec_from_data_attrib(queue_attrib_array[i]));
-            }
-        }
-        if (queue_array.length > 0) {
-            return queue_array;
-        } else {
-            return [];
-        }
-    }
-
-    function clear_queue() {
-        $('#capture_queue_builder').html('');
+    var cq_util = window.ff.cq_util;
+    
+    cq_util.clear_queue = function() {
+        $('#capture_job_queue').html('');
         $('input#queue_name').val('');
         $('textarea#queue_comment').val('');
-        no_jobs_placeholder();
+        cq_util.no_jobs_placeholder();
         window.ff.cjq_id = 0;
-    }
-
-    function no_jobs_placeholder() {
-        if (get_queue_array().length == 0) {
-            $('#capture_queue_builder').html(
-                '<li id="queue_placeholder">No queued jobs.</li>'
-            );
-        }
-    }
-
-    function load_queue(id) {
-        clear_queue();
-        var cjq = $('#capture_queue_builder');
-        cjq.html('');
-
-        window.ff.cjq_id = id;
-        var queue = window.ff.cjqs[id];
-        var queue_array = queue.queue;
-
-        $('input#queue_name').val(queue.name);
-        $('textarea#queue_comment').val(queue.comment);
-        for (var j in queue_array) {
-            var job_spec = queue_array[j];
-            cjq.append(li_from_job_spec(job_spec));
-        }
     }
 
     function delete_queue(id) {
@@ -138,7 +24,7 @@ $(document).ready(function(){
             url: window.ff.cjq_saver_url,  // set by inline javascript on the main page
             data: { payload_json: JSON.stringify(payload) },
             success: function (data, status, jqXHR) {
-                refresh_queues();
+                cq_util.refresh_queues();
             },
             dataType: 'json'
         });
@@ -150,7 +36,7 @@ $(document).ready(function(){
      * Add some jQuery UI magic
      */
 
-    $('#capture_queue_builder').sortable({
+    $('#capture_job_queue').sortable({
         tolerance: 'pointer',
         cursor: 'pointer',
         revert: false,
@@ -171,7 +57,7 @@ $(document).ready(function(){
 
         update: function(event, ui) {
             $('#queue_placeholder').remove();
-            no_jobs_placeholder();
+            cq_util.no_jobs_placeholder();
         },
         stop: function(event, ui) {
             if (ui.item.hasClass("fresh_cjt")) {
@@ -182,7 +68,7 @@ $(document).ready(function(){
         beforeStop: function(event, ui) {
             if (keep_job_in_queue == 0) {
                 ui.item.remove();
-                no_jobs_placeholder();
+                cq_util.no_jobs_placeholder();
             } else {
                 newItem = ui.item;
                 attrib_job_spec = ui.helper.attr('data-attrib_job_spec');
@@ -195,7 +81,7 @@ $(document).ready(function(){
     });
 
     $(".fresh_cjt").draggable({
-        connectToSortable: "#capture_queue_builder",
+        connectToSortable: "#capture_job_queue",
         helper: "clone",
         revert: false
     });
@@ -205,17 +91,17 @@ $(document).ready(function(){
      */
 
     $('#cjq_list').on('click', '.queue_loader', function() {
-        load_queue($(this)[0].id.split('_')[1]);
+        cq_util.load_queue($(this)[0].id.split('_')[1]);
     });
 
     $('#cjq_list').on('click', '.queue_deleter', function() {
         delete_queue($(this)[0].id.split('_')[1]);
     });
 
-    $('#clear_queue_button').click(clear_queue);
+    $('#clear_queue_button').click(cq_util.clear_queue);
 
     $("#save_queue_button").click(function() {
-        var queue_array = get_queue_array();
+        var queue_array = cq_util.get_queue_array();
 
         if (queue_array.length > 0 || window.ff.queue_id > 0) {
 
@@ -248,7 +134,7 @@ $(document).ready(function(){
 
                     }
 
-                    refresh_queues();
+                    cq_util.refresh_queues();
                 },
                 error: function(jqXHR, status, error) {
 
@@ -264,11 +150,11 @@ $(document).ready(function(){
     for (idx in window.ff.cjt_ids) {
         var cjt_id = window.ff.cjt_ids[idx];
         var cjt = $('#CJT_' + cjt_id);
-        cjt.attr('data-attrib_job_spec', data_attrib_from_job_spec(window.ff.job_specs[cjt_id]));
-        cjt.html(inner_li_from_job_spec(window.ff.job_specs[cjt_id]));
+        cjt.attr('data-attrib_job_spec', cq_util.data_attrib_from_job_spec(window.ff.job_specs[cjt_id]));
+        cjt.html(cq_util.inner_li_from_job_spec(window.ff.job_specs[cjt_id]));
     }
 
-    refresh_queues();
+    cq_util.refresh_queues();
 
     var main_loop = window.setInterval(
         function() {  // executed once per second to update displays with timers
---
+++ django_fishface/djff/static/djff/cq_utils.js
@@ -0,0 +1,148 @@
+$(document).ready(function(){
+    var cq_util = {};
+
+    cq_util.data_attrib_from_job_spec = function(job_spec) {
+        return job_spec.voltage + "_" +
+               job_spec.current + "_" +
+               job_spec.startup_delay + "_" +
+               job_spec.interval + "_" +
+               job_spec.duration;
+    }
+
+    cq_util.job_spec_from_data_attrib = function(data_attrib) {
+        var job_spec_array = data_attrib.split("_");
+        var job_spec = {
+            voltage: job_spec_array[0],
+            current: job_spec_array[1],
+            startup_delay: job_spec_array[2],
+            interval: job_spec_array[3],
+            duration: job_spec_array[4]
+        };
+
+        return job_spec;
+    }
+
+    cq_util.li_from_queue = function(queue) {
+        return '<li class="saved_queue" id="CJQ_' + queue.id + '">' +
+            'Name: ' + queue.name + '<br>' +
+            'Description: ' + queue.comment + '<br><br>' +
+            '<span class="boxed queue_loader" id="CJQ_' + queue.id + '_LOAD">&larr;load</span> ' +
+            '<span class="boxed queue_deleter" id="CJQ_' + queue.id + '_DEL">delete</span>' +
+            '</li>';
+    }
+
+    cq_util.li_from_job_spec = function(job_spec) {
+        return '<li class="job_queue_item" data-attrib_job_spec=' + cq_util.data_attrib_from_job_spec(job_spec) + '>' +
+               cq_util.inner_li_from_job_spec(job_spec) +
+               '</li>';
+    }
+
+    cq_util.inner_li_from_job_spec = function(job_spec) {
+        var inner_li;
+        if (job_spec.interval > 0) {
+            inner_li =
+                'Set (' + job_spec.voltage + 'V, ' + job_spec.current + 'A), ' +
+                'wait ' + job_spec.startup_delay + 's, ' +
+                'then <br /> capture every ' +
+                job_spec.interval + 's for ' + job_spec.duration + 's';
+        } else {
+            inner_li = 'Set (' + job_spec.voltage + 'V, ' + job_spec.current + 'A) ' + 'for ' + job_spec.duration + 's';
+        }
+        return inner_li;
+    }
+
+    cq_util.refresh_queues = function() {
+        $.ajax({
+            type: 'GET',
+            url: window.ff.cjqs_url,  // set by inline javascript on the main page
+            success: function (data, status, jqXHR) {
+                window.ff.cjqs = data.cjqs;
+                window.ff.cjq_ids = data.cjq_ids;
+                var cjq_list = $('#cjq_list');
+                cjq_list.html('');
+                if (data.cjq_ids.length > 0) {
+                    for (var i in data.cjq_ids) {
+                        var queue = data.cjqs[data.cjq_ids[i]];
+                        cjq_list.append(cq_util.li_from_queue(queue));
+                    }
+                } else {
+                    cjq_list.html('<li>No saved queues.<li>');
+                }
+                
+            },
+            error: function(jqXHR, status, error) {
+
+            },
+            dataType: 'json'
+        });
+    }
+
+    cq_util.get_queue_array = function() {
+        var queue_attrib_array = $('#capture_job_queue').sortable('toArray', {'attribute': 'data-attrib_job_spec'});
+        var queue_array = [];
+        for (var i in queue_attrib_array) {
+            if (queue_attrib_array[i].length > 0) {
+                queue_array.push(cq_util.job_spec_from_data_attrib(queue_attrib_array[i]));
+            }
+        }
+        if (queue_array.length > 0) {
+            return queue_array;
+        } else {
+            return [];
+        }
+    }
+
+    cq_util.no_jobs_placeholder = function() {
+        if (cq_util.get_queue_array().length == 0) {
+            $('#capture_queue_builder').html(
+                '<li id="queue_placeholder">No queued jobs.</li>'
+            );
+        }
+    }
+
+    cq_util.load_queue = function(id) {
+        cq_util.clear_queue();
+        var cjq = $('#capture_queue_builder');
+        cjq.html('');
+
+        window.ff.cjq_id = id;
+        var queue = window.ff.cjqs[id];
+        var queue_array = queue.queue;
+
+        $('input#queue_name').val(queue.name);
+        $('textarea#queue_comment').val(queue.comment);
+        for (var j in queue_array) {
+            var job_spec = queue_array[j];
+            cjq.append(cq_util.li_from_job_spec(job_spec));
+        }
+    }
+
+    cq_util.delete_queue = function(id) {
+        if (window.ff.cjq_id == id) {
+            window.ff.cjq_id = 0;
+        }
+
+        var payload = {
+            cjq_id: id,
+            delete: 1
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: window.ff.cjq_saver_url,  // set by inline javascript on the main page
+            data: { payload_json: JSON.stringify(payload) },
+            success: function (data, status, jqXHR) {
+                cq_util.refresh_queues();
+            },
+            dataType: 'json'
+        });
+    }
+
+    window.ff.cq_util = cq_util;
+
+    $('#cjq_list').on('click', '.queue_loader', function() {
+        cq_util.load_queue($(this)[0].id.split('_')[1]);
+    });
+
+
+});  // End of document.ready()
