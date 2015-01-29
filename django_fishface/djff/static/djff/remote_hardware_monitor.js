3b8c63e4a43025926cf2ef42ada6f2df19b2b0ba
Wil Langford <wil.langford+github@gmail.com>
2015-01-29 06:59:04 -0800

    Changed timeout of remote hardware monitor to much shorter than default.

--- django_fishface/djff/static/djff/remote_hardware_monitor.js
+++ django_fishface/djff/static/djff/remote_hardware_monitor.js
@@ -8,6 +8,17 @@ $(document).ready(function() {
             type: 'POST',
             url: window.ff.telemetry_proxy_url,  // set by inline javascript on the main page
             data: payload,
+            timeout: 2000,
+            error: function (jqXHR, status, error) {
+                $('#RASPI').css('background-color', 'red');
+                $('#CJC').css('background-color', 'gray');
+                $('#DP').css('background-color', 'gray');
+                if (status != 'timeout') {
+                    console.log('non-timeout error on hardware monitor request: ' + status);
+                    console.log(jqXHR);
+                    console.log(error);
+                }
+            },
             success: function (data, status, jqXHR) {
                 if (data.command=='raspi_monitor') {
                     $('#RASPI').css('background-color', 'lightgreen');
@@ -20,7 +31,6 @@ $(document).ready(function() {
 
                     var thr_status_element_id = {
                         capturejob_controller: 'CJC',
-                        current_framer: 'CF',
                         deathcry_publisher: 'DP'
                     }[thr.name];
 
