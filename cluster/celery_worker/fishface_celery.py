8a947d67ad9d331d39b22c2933ee4415979361cb
Wil Langford <wil.langford+github@gmail.com>
2015-02-23 09:29:16 -0800

    Some more tweaks to authentication.

--- cluster/celery_worker/fishface_celery.py
+++ cluster/celery_worker/fishface_celery.py
@@ -9,9 +9,10 @@ with open(os.path.join(ALT_ROOT, 'etc', 'redis', 'redis_password'), 'rt') as f:
 with open(os.path.join(ALT_ROOT, 'var', 'run', 'redis.hostname'), 'rt') as f:
     hostname = f.read().strip()
 
-redis_url = 'redis://:{}@{}'.format(password, hostname)
+broker_url = 'redis://:{}@{}'.format(password, hostname)
+result_url = 'redis://{}'.format(hostname)
 
-app = celery.Celery('tasks', backend=redis_url, broker=redis_url)
+app = celery.Celery('tasks', backend=result_url, broker=broker_url)
 app.conf.CELERY_ACCEPT_CONTENT = ['pickle', 'json']
 app.conf.CELERY_TASK_SERIALIZER = 'pickle'
 app.conf.CELERY_RESULT_SERIALIZER = 'pickle'
