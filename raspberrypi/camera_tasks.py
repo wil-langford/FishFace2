9aa10fcbd2a252170e775e8b1a2768245de9f451
Wil Langford <wil.langford+github@gmail.com>
2015-03-03 12:25:34 -0800

    Better queueing with Queue instead of heapq.

--- raspberrypi/camera_tasks.py
+++ raspberrypi/camera_tasks.py
@@ -1,7 +1,7 @@
 import os
 import time
 import io
-import heapq
+import Queue
 
 import threading
 
@@ -67,7 +67,7 @@ class ThreadWithHeartbeat(threading.Thread):
     def __init__(self, heartbeat_interval=0.2, heartbeat_publish_interval=None, *args, **kwargs):
         super(ThreadWithHeartbeat, self).__init__(*args, **kwargs)
 
-        self.name = None
+        self._name = None
 
         self._heartbeat_count = 0
         self._heartbeat_timestamp = None
@@ -141,9 +141,23 @@ class ThreadWithHeartbeat(threading.Thread):
         except TypeError:
             return None
 
-    def abort(self):
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if self._name is None:
+            self._name = new_name + "." + str(time.time())
+        else:
+            logger.warning("Tried to rename thread.  Thread names are immutable once set.")
+
+    def abort(self, complete=False):
         self._keep_looping = False
-        logger.info('Thread {} aborted.'.format(self.name))
+        if complete:
+            logger.info('Thread {} complete.  Shutting down.'.format(self.name))
+        else:
+            logger.warning('Thread {} aborted.'.format(self.name))
 
 
 class CaptureThread(ThreadWithHeartbeat):
@@ -151,30 +165,34 @@ class CaptureThread(ThreadWithHeartbeat):
         super(CaptureThread, self).__init__(*args, **kwargs)
         self.name = 'capture_thread'
 
-        self._heap = []
-        self._heap_lock = threading.RLock()
+        self.queue = Queue.PriorityQueue()
+
+        self._wait_for_capture_when_less_than = self._heartbeat_interval * 3
 
         self.cam = Camera()
 
-        self._next_capture = None
+        self._next_capture_time = None
 
     def _heartbeat_run(self):
-        if self._next_capture is None:
-            self._next_capture = self.pop_next_capture()
+        if self._next_capture_time is None:
+            self._next_capture_time = self.pop_next_request()
 
-        if self._next_capture - time.time() < self._heartbeat_interval * 2:
-            delay_until(self._next_capture)
+        if not self._keep_looping:
+            return
+
+        if self._next_capture_time - time.time() < self._wait_for_capture_when_less_than:
+            delay_until(self._next_capture_time)
             stream, timestamp = self.cam.get_image_with_capture_time()
 
             image = stream.read()
 
             celery_app.send_task('results.post_image', kwargs={
                 'image': image,
-                'requested_timestamp': self._next_capture,
+                'requested_timestamp': self._next_capture_time,
                 'actual_timestamp': timestamp
             })
 
-            self._next_capture = None
+            self._next_capture_time = None
 
     def _pre_run(self):
         self.set_ready()
@@ -183,12 +201,19 @@ class CaptureThread(ThreadWithHeartbeat):
         pass
 
     def push_capture_request(self, requested_capture_timestamp):
-        with self._heap_lock:
-            heapq.heappush(self._heap, requested_capture_timestamp)
+        self.queue.put(requested_capture_timestamp)
 
-    def pop_next_capture(self):
-        with self._heap_lock:
-            return heapq.heappop(self._heap)
+    def pop_next_request(self):
+        try:
+            self.queue.get(block=False)
+        except Queue.Empty:
+            self.abort(complete=True)
+
+    def abort(self, complete=False):
+        super(CaptureThread, self).abort(complete=complete)
+        self.cam.close()
+        self.cam = None
+
 
 
 @celery.shared_task(name="camera.push_capture_request")
