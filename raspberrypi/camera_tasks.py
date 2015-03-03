20b4751de90e188f58a2d72e382056255e227aec
wil-langford <wil.langford+github@gmail.com>
2015-03-03 09:07:13 -0800

    Started writing new celery-enabled scripts for hardware control.

---
+++ raspberrypi/camera_tasks.py
@@ -0,0 +1,63 @@
+import os
+import time
+
+import threading
+
+import celery
+from fishface_celery import app as celery_app
+
+from raspi_logging import logger
+
+REAL_HARDWARE = not os.path.isfile('FAKE_THE_HARDWARE')
+
+class Camera(object):
+    def __init__(self, real=True, resolution=(2048, 1536), rotation=180):
+        self._lock = threading.RLock()
+        self.cam = None
+
+        self.resolution = resolution
+        self.rotation = rotation
+
+        self.close_if_greater_than = 30
+        self.open_if_less_than =
+
+        if REAL_HARDWARE:
+            logger.info('Running with real power supply.')
+            import picamera
+            self.cam_class = picamera.PiCamera
+        else:
+            logger.warning('Running with fake power supply.')
+            import FakeHardware
+            self.cam_class = FakeHardware.PiCamera
+
+    def open(self):
+        if self.cam is not None:
+            return False
+
+        self.cam = self.cam_class()
+        self.cam.resolution = self.resolution
+        self.cam.rotation = self.rotation
+
+        return True
+
+    def close(self):
+        if self.cam is None:
+            return False
+
+        self.cam = None
+
+        return True
+
+camera = Camera(real=REAL_HARDWARE)
+
+# Move this into the yet-to-be-implemented scheduler
+# camera.open()
+
+
+@celery.shared_task(name="psu.post_image")
+def post_image(meta):
+    pass
+
+
+class PowerSupplyError(Exception):
+    pass
