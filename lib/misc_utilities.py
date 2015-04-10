b47bb95b8096eb7b6d140ddf2e67d700ff4c450d
Wil Langford <wil.langford+github@gmail.com>
2015-04-10 09:06:01 -0700

    Machine-learning tweaks.

--- lib/misc_utilities.py
+++ lib/misc_utilities.py
@@ -33,11 +33,25 @@ def return_text_file_contents(file_path, strip=True, ignore_fail=True):
             logging.warning("Couldn't read file: {}".format(file_path))
         return ''
 
+
 def chunkify(chunkable, chunk_length=1):
     while chunkable:
         chunk = chunkable[:chunk_length]
         chunkable = chunkable[chunk_length:]
         yield chunk
 
+
 def is_file(*args):
     return os.path.isfile(os.path.join(*args))
+
+
+def n_chunkify(n, chunkable):
+    """
+    Split chunkable into (roughly) n chunks.
+    """
+    chunk_length = float(len(chunkable)) / n
+    for i in xrange(0, n-1):
+        start = int(i*chunk_length)
+        stop = int((i+1)*chunk_length)
+        yield chunkable[start:stop]
+    yield chunkable[int((n-1)*chunk_length):]
--- lib/workers/learn_tasks.py
+++ lib/workers/learn_tasks.py
@@ -3,6 +3,9 @@ import sklearn.cluster as skc
 import celery
 from lib.fishface_celery import celery_app
 
+import etc.fishface_config as ff_conf
+from lib.misc_utilities import n_chunkify
+
 @celery.shared_task(bind=True, name='learn.debug_task')
 def debug_task(self, *args, **kwargs):
     return '''
@@ -12,8 +15,8 @@ def debug_task(self, *args, **kwargs):
     '''.format(self.request, args, kwargs)
 
 
-@celery.shared_task(name="learn.cluster_hu_moments")
-def cluster_hu_moments(hu_moments, n_clusters=80, n_init=10, init='k-means++'):
+@celery.shared_task(name="learn.create_estimator")
+def create_estimator(hu_moments, n_clusters=80, n_init=10, init='k-means++'):
     try:
         if len(hu_moments[0]) != 7:
             raise ClusteringError("Hu moments contain 7 elements - the passed object doesn't fit" +
@@ -25,9 +28,7 @@ def cluster_hu_moments(hu_moments, n_clusters=80, n_init=10, init='k-means++'):
     data = skp.scale(hu_moments)
     estimator = skc.KMeans(init=init, n_clusters=n_clusters, n_init=n_init)
 
-    predictions = estimator.fit_predict(data)
-
-    return predictions, estimator
+    return estimator
 
 
 class ClusteringError(Exception):
