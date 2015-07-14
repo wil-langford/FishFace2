51ec323fc51f9584cee4eb8f95e2150cd044faa2
wil-langford <wil.langford+github@gmail.com>
2015-07-14 15:39:53 -0700

    Fixed bug in remote-to-local filename converter.

--- lib/cluster_utilities.py
+++ lib/cluster_utilities.py
@@ -7,9 +7,17 @@ import numpy as np
 def remote_to_local_filename(remote_filename, local_media_parent=None):
     if local_media_parent is None:
         local_media_parent = cl_conf.LOCAL_CACHE_DIR
+
+    media_location = remote_filename.find('media')
+    if media_location > -1:
+        media_location += 6
+
+    start_index = max(0, media_location)
+
     return os.path.join(
         local_media_parent,
-        remote_filename[remote_filename.find('media'):]
+        'media',
+        remote_filename[start_index:]
     )
 
 
